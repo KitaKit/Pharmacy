7b015f94906c2f30cf8b54a749b279d12ad8a1d4
Nikita <sositeherstupidmans@gmail.com>
2023-03-22 18:51:49 +0100

    Pharmacy

--- Lekarna/MainWindow.xaml.cs
+++ Lekarna/MainWindow.xaml.cs
@@ -14,6 +14,46 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
+/*
+--------------------Список того, что работает прямо сейчас:--------------------
+
+*** Общее
+ - Отображение данных в окне приложения (класс DataLists метод ShowDataToDataGrid())
+ - Метод при нажатии на кнопку Load from DataBase в верхнем левом меню преложения
+ - Метод при нажатии на кнопку Load from CSV-file в верхнем левом меню преложения
+ - Метод при нажатии на кнопку Save to new CSV-file в верхнем левом меню преложения
+ 
+*** Database
+ - Подключение к базе данных (класс DatabaseConnectionService)
+ - Считывание данных из база данных (класс DatabaseLogic)
+ - Сохранение данных в соответствующие списки (класс DatabaseLogic с помощью метода AddToDataList() из класса DataLists)
+ 
+*** File
+ - Подключение к CSV-файлам (класс FileConnectionService)
+ - Считывание данных из отдельных файлов для каждой отдельной таблицы (класс FileIOLogic)
+ - Сохранение данных в соответствующий список из соответствующего файла (класс FileIOLogic)
+ - Запись данных из соответствующего списка в соответствующий !!!НОВЫЙ!!! файл (класс FileIOLogic)
+
+--------------------Seznam toho, co právě teď funguje:--------------------
+
+*** General
+ - Zobrazení dat v okně aplikace (metoda třídy DataLists ShowDataToDataGrid())
+ - Metoda při stisknutí tlačítka "Load from DataBase" v levém horním menu aplikace
+ - Metoda při stisknutí tlačítka "Load from CSV-file" v levém horním menu aplikace
+ - Metoda při stisknutí tlačítka "Save to new CSV-file" v levém horním menu aplikace
+
+*** Database
+ - Připojení k databázi (třída DatabaseConnectionService)
+ - Načtení dat z databáze (třída DatabaseLogic)
+ - Uložení dat do příslušných seznamů (třída DatabaseLogic pomocí metody AddToDataList() ze třídy DataLists)
+
+*** File
+ - Připojení k souborům CSV (třída FileConnectionService)
+ - Čtení dat z jednotlivých souborů pro každou jednotlivou tabulku (třída FileIOLogic)
+ - Ukládání dat do příslušného seznamu z příslušného souboru (třída FileIOLogic)
+ - Zápis dat z příslušného seznamu do příslušného !!!NOVÉHO!!! souboru (třída FileIOLogic)
+ */
+
 
 //Здесь будет описана логика основных взаимодействий с окном программы и его содержимым
 
@@ -34,7 +74,7 @@ namespace Pharmacy
         }
 
         private void menuItemLoadFromDataBase_Click(object sender, RoutedEventArgs e) //при нажатии на пункт меню "Load from DataBase" (Menu слева сверху) вызывается данный метод и происходит подключение и считывание данных из базы данных с помощью класса DatabaseLogic и метода LoadData()
-                                                                                     // Kliknutím na položku menu "Load from DataBase" (Menu vlevo nahoře) se vyvolá tato metoda a připojí se a načte data z databáze pomocí třídy DatabaseLogic a metody LoadData()
+                                                                                      // Kliknutím na položku menu "Load from DataBase" (Menu vlevo nahoře) se vyvolá tato metoda a připojí se a načte data z databáze pomocí třídy DatabaseLogic a metody LoadData()
         {
             DatabaseIOLogic dataBase = new DatabaseIOLogic();
             dataBase.ReadData(mainDataLists);
@@ -81,42 +121,3 @@ namespace Pharmacy
         }
     }
 }
-/*
---------------------Список того, что работает прямо сейчас:--------------------
-
-*** Общее
- - Отображение данных в окне приложения (класс DataLists метод ShowDataToDataGrid())
- - Метод при нажатии на кнопку Load from DataBase в верхнем левом меню преложения
- - Метод при нажатии на кнопку Load from CSV-file в верхнем левом меню преложения
- - Метод при нажатии на кнопку Save to new CSV-file в верхнем левом меню преложения
- 
-*** Database
- - Подключение к базе данных (класс DatabaseConnectionService)
- - Считывание данных из база данных (класс DatabaseLogic)
- - Сохранение данных в соответствующие списки (класс DatabaseLogic с помощью метода AddToDataList() из класса DataLists)
- 
-*** File
- - Подключение к CSV-файлам (класс FileConnectionService)
- - Считывание данных из отдельных файлов для каждой отдельной таблицы (класс FileIOLogic)
- - Сохранение данных в соответствующий список из соответствующего файла (класс FileIOLogic)
- - Запись данных из соответствующего списка в соответствующий !!!НОВЫЙ!!! файл (класс FileIOLogic)
-
---------------------Seznam toho, co právě teď funguje:--------------------
-
-*** General
- - Zobrazení dat v okně aplikace (metoda třídy DataLists ShowDataToDataGrid())
- - Metoda při stisknutí tlačítka "Load from DataBase" v levém horním menu aplikace
- - Metoda při stisknutí tlačítka "Load from CSV-file" v levém horním menu aplikace
- - Metoda při stisknutí tlačítka "Save to new CSV-file" v levém horním menu aplikace
-
-*** Database
- - Připojení k databázi (třída DatabaseConnectionService)
- - Načtení dat z databáze (třída DatabaseLogic)
- - Uložení dat do příslušných seznamů (třída DatabaseLogic pomocí metody AddToDataList() ze třídy DataLists)
-
-*** File
- - Připojení k souborům CSV (třída FileConnectionService)
- - Čtení dat z jednotlivých souborů pro každou jednotlivou tabulku (třída FileIOLogic)
- - Ukládání dat do příslušného seznamu z příslušného souboru (třída FileIOLogic)
- - Zápis dat z příslušného seznamu do příslušného !!!NOVÉHO!!! souboru (třída FileIOLogic)
- */
